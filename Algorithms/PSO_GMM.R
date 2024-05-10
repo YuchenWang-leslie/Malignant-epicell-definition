546854d33274e06f0dbbf1cbf2623dad8919ea3a
Leslie-Wang <128453006+YuchenWang-leslie@users.noreply.github.com>
2024-05-10 14:18:10 +0800

    Add files via upload

--- Algorithms/PSO_GMM.R
+++ Algorithms/PSO_GMM.R
@@ -15,14 +15,14 @@ DEG <- read.csv("~/wyc_20231021/Python/GMM_mixture/DEG_new.csv",row.names = 1)
 
 ### set the parameters
 seurat_object <- epicell
-size <- 30
+size <- 50
 poolrange <- 500
-generation <- 30
+generation <- 50
 toprange <- 50
 dim <- toprange*2
 vmax <- 2
-c1 <- 2
-c2 <- 2
+c1 <- 1
+c2 <- 1
 r1 <- runif(1)
 r2 <- runif(1)
 DEG_list <- DEG$genename
@@ -34,17 +34,17 @@ certainty <- 0.01
 ###############    creat the initial population and velocities   ###############
 
 # create individual 
-create_individual <- function(poolrange, DEG_range) {
+create_individual <- function(poolrange, DEG_range,size,toprange) {
   numbers1 <- 1:poolrange
-  weights1 <- exp(-1e-5 * (numbers1 - 1)^2)
+  weights1 <- exp(-1e-4 * (numbers1 - 1)^2)+1
   weights1 <- weights1 / sum(weights1) # scale the weight
-  sample1 <- sample(numbers1, size = 50, replace = FALSE, prob = weights1)
+  sample1 <- sample(numbers1, size = toprange, replace = FALSE, prob = weights1)
   sample1 <- sort(sample1)
   
   numbers2 <- DEG_range:(DEG_range - poolrange + 1)
-  weights2 <- exp(-1e-5 * (DEG_range - numbers2)^2)
+  weights2 <- exp(-1e-4 * (DEG_range - numbers2)^2)+1
   weights2 <- weights2 / sum(weights2) # scale the weight
-  sample2 <- sample(numbers2, size = 50, replace = FALSE, prob = weights1)
+  sample2 <- sample(numbers2, size = toprange, replace = FALSE, prob = weights1)
   sample2 <- sort(sample2)
   
   individual <- c(sample1, sample2)
@@ -52,16 +52,16 @@ create_individual <- function(poolrange, DEG_range) {
 }
 
 # creat the initial population 
-initial_population <- function(poolrange, DEG_range, size) {
-  population_matrix <- matrix(nrow = size, ncol = 100)
+initial_population <- function(poolrange, DEG_range, size,dim,toprange) {
+  population_matrix <- matrix(nrow = size, ncol = dim)
   for (i in 1:size) {
-    individual <- create_individual(poolrange, DEG_range)
+    individual <- create_individual(poolrange, DEG_range,size,toprange)
     population_matrix[i, ] <- individual
   }
   return(population_matrix)
 }
 
-initial_population_matrix <- initial_population(poolrange, DEG_range, size)
+initial_population_matrix <- initial_population(poolrange, DEG_range, size,dim,toprange)
 
 ###############################################################################-
 ######################    creat the initial velocities #########################
@@ -73,7 +73,7 @@ velocities <- matrix(runif(dim * size, -vmax, vmax), ncol = dim)
 ###############################################################################-
 ######################    get fitness function  ################################
 
-#evaluate class by WGSS & BGSS
+#evaluate class by WGSS & OGSS
 eva_ss <- function(data) {
   # every cluster's center
   centroids <-
@@ -86,15 +86,14 @@ eva_ss <- function(data) {
     sum((data[data$class == class, 1] - centroids[class, "centroid_x"]) ^ 2 +
           (data[data$class == class, 2] - centroids[class, "centroid_y"]) ^2)
   }))
-  # overall center
-  overall_centroid <- colMeans(data[, 1:2])
-  # BGSS
-  BGSS <- sum(sapply(unique(data$class), function(class) {
-    n_cluster <- sum(data$class == class)
-    sum((centroids[class, "centroid_x"] - overall_centroid[1]) ^ 2 +
-          (centroids[class, "centroid_y"] - overall_centroid[2]) ^ 2) * n_cluster
+
+  # OGSS
+  OGSS <- sum(sapply(unique(data$class), function(class) {
+    exclude_data <- data[data$class != class,]
+    sum((exclude_data[, 1] - centroids[class, "centroid_x"]) ^ 2 +
+          (exclude_data[, 2] - centroids[class, "centroid_y"]) ^ 2)
   }))
-  fitness <-  BGSS-WGSS 
+  fitness <- as.numeric((OGSS - WGSS) / pmax(WGSS, OGSS)+ dist(centroids))
   return(fitness)
 }
 
@@ -136,22 +135,24 @@ population_matrix <- initial_population_matrix
 ##############################   show result   #################################
 result_matrix <- matrix(ncol = 2,nrow = generation)
 
-# PSO MAIN FUNCTION
+# PSO
 for (gen in 1:generation) {
   for (i in 1:size) {
-    # update individual position
+    # update personal position 
     population_matrix[i,] <- round(population_matrix[i,] + velocities[i,])
+    population_matrix[population_matrix > DEG_range] <- DEG_range
+    population_matrix[population_matrix < 0] <- 0
     
-    # get fitness for every individual
+    # get fitness
     fitness <- get_fitness(population_matrix[i,], DEG_list, seurat_object,toprange,dim,certainty)
     
-    # update individual best fitness
+    # update personal best fitness
     if (fitness > pBest_fitness[i]) {
       pBest_fitness[i] <- fitness
       pBest[i,] <- population_matrix[i,]
     }
     
-    # update globle best fitness
+    # update globel best fitness
     if (fitness > gBest_fitness) {
       gBest_fitness <- fitness
       gBest <- population_matrix[i,]
@@ -166,9 +167,11 @@ for (gen in 1:generation) {
   velocities[velocities < (-vmax)] <- (-vmax)
 
   cat("Generation:", gen, " gBestFitness:", gBest_fitness, "\n")
+  print(pBest_fitness)
   result_matrix[gen,] <- c(gen,gBest_fitness)
-  return(result_matrix)
+  
 }
+print(result_matrix)
 
 cat("Global Best Solution:\n")
 print(gBest)
